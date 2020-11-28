93ad818619d3d1c10d097c8bd8e89dce5e587666
Alonso Garcia garcia <alogarci@c2r7s2.42madrid.com>
2020-11-28 16:57:32 +0100

    beginRush00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alogarci <alogarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/28 13:04:09 by alogarci          #+#    #+#             */
+/*   Updated: 2020/11/28 15:33:00 by alogarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_print_comb()
+{
+	int a;
+	int b;
+	int c;
+
+	a = '0' - 1;
+
+	while (++a <= '9')
+	{
+		b = a;
+		while (++b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (a != '7' || b != '8' || c != '9')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+			}
+		}
+	}
+}
