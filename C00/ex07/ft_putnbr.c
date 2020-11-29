70d46008a04c1f6a8abe1ebee172a32b57b7363f
Alonso Garcia garcia <alogarci@c2r7s2.42madrid.com>
2020-11-29 21:58:16 +0100

    endDay07

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alogarci <alogarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/29 20:40:34 by alogarci          #+#    #+#             */
+/*   Updated: 2020/11/29 20:57:36 by alogarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if ((nb > -2147483648) || (nb <= 2147483647))
+	{
+		if (nb == -2147483648)
+		{
+			ft_putchar('-');
+			ft_putchar('2');
+			ft_putnbr(147483648);
+			return ;
+		}
+		else if (nb >= 10)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else if (nb < 0)
+		{
+			nb = -nb;
+			ft_putchar('-');
+			ft_putnbr(nb);
+		}
+		else
+		{
+			ft_putchar(nb + '0');
+		}
+	}
+}
