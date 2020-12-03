608de32adb2d338e0f22f3fbaacace8477b0e55b
Alonso Garcia garcia <alogarci@c2r7s2.42madrid.com>
2020-12-03 17:30:08 +0100

    endDay11

---
+++ C02/ex09/ft_strcapitalize_BAD.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize_BAD.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alogarci <alogarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 11:40:23 by alogarci          #+#    #+#             */
+/*   Updated: 2020/12/03 16:47:46 by alogarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int n;
+	int check;
+
+	check = 0;
+	n = 0;
+	while (str[n] != '\0')
+	{
+		if (n == 0 | (str[n] >= ' ' && str[n] < '/') || (str[n] >= ':' && str[n] <= '@'))
+			check = 0;
+		else if (str[n] <= 0 && str[n])
+			check = 1;
+		else if (str[n] <= 'A' && str[n] >= 'Z')
+			check = 1;
+		else if (str[n] >= 'a' && str[n] <= 'z' && check == 0)
+			str[n] -= 32;
+		n++;
+		check = 1;
+	}
+	return (*str);
+}
